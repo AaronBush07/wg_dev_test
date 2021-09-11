09c670420022b880fb3ec225ad15a43162211efa
Aaronbush07 <Aaron.bush07@gmail.com>
2021-09-12 00:20:57 +1000

    added 200 response test

--- test/index.test.js
+++ test/index.test.js
@@ -3,9 +3,9 @@ const http = require("http");
 const supertest = require("supertest");
 const request = supertest(app);
 const db = require("../db");
-const validate = require('../validation/schema');
+const validate = require("../validation/schema");
 const backup = db.backup();
-const qs = require('qs')
+const qs = require("qs");
 
 it("1 should equal 1", () => {
   expect(1).toBe(1);
@@ -17,59 +17,62 @@ describe("Endpoint responses", () => {
     expect(response.status).toBe(404);
   });
 
-  it('Return 400', async() => {
+  it("Return 400", async () => {
     const str = qs.stringify({
-      pricemin: 400, 
-      pricemax: 200
+      pricemin: 400,
+      pricemax: 200,
     });
-    const response = await (request.get(`/products?${str}`))
-    expect(response.status).toBe(400)
-  })
+    const response = await request.get(`/products?${str}`);
+    expect(response.status).toBe(400);
+  });
 
+  it("Return 200", async () => {
+    const response = await request.get("/products");
+    expect(response.status).toBe(200);
+  });
 });
 
-
-describe("Schema validation", ()=> {
-  it("Validate schema", async ()=> {
+describe("Schema validation", () => {
+  it("Validate schema", async () => {
     const data = {
       pricemin: 100,
       pricemax: 200,
       fantastic: true,
-      rating: 3
-    }
-    const result = await validate(data)
-    expect(result.error).toBe(undefined)
-  })
+      rating: 3,
+    };
+    const result = await validate(data);
+    expect(result.error).toBe(undefined);
+  });
 
-  it("Pricemin must be less than pricemax", async ()=> {
+  it("Pricemin must be less than pricemax", async () => {
     const data = {
       pricemin: 400,
       pricemax: 200,
       fantastic: true,
-      rating: 3
-    }
-    const result = await validate(data)
+      rating: 3,
+    };
+    const result = await validate(data);
     expect(result.error).toBeTruthy();
-  })
+  });
 
-  it("Not all fields compulsory", async ()=> {
+  it("Not all fields compulsory", async () => {
     const data = {
       pricemax: 200,
-      rating: 3
-    }
+      rating: 3,
+    };
     const result = await validate(data);
-    console.log(result)
+    console.log(result);
     expect(result.error).toBe(undefined);
-  })
+  });
 
-  it("Bad schema", async ()=> {
+  it("Bad schema", async () => {
     const data = {
-      pricemin: 'aaaa',
-      pricemax: 'bbb',
+      pricemin: "aaaa",
+      pricemax: "bbb",
       fantastic: true,
-      rating: 3
-    }
-    const result = await validate(data)
-    expect(result.error).toBeTruthy()
-  })
-})
+      rating: 3,
+    };
+    const result = await validate(data);
+    expect(result.error).toBeTruthy();
+  });
+});

ab412564fe563cc7a65270617070f55c20652d8d
Aaronbush07 <Aaron.bush07@gmail.com>
2021-09-12 01:21:53 +1000

    cleanup code and added pagination

--- routes/index.js
+++ routes/index.js
@@ -18,34 +18,17 @@ router.use("/products", async function (req, res, next) {
 router.get("/products", function (req, res) {
   try {
     const where = whereParser(req.query);
-    let query = `SELECT * FROM PRODUCTS ${where? 'WHERE '+ where: ''}`
-    console.log(query)
+    let query = `SELECT * FROM PRODUCTS ${where ? "WHERE " + where : ""} ORDER BY ID ASC `;
+    if (req.query.offset) {
+      query += `OFFSET ${req.query.offset} ROWS `
+    }
+    if (req.query.limit) {
+      query += `FETCH NEXT ${req.query.limit} ROWS`
+    }
     res.status(200).send(db.public.many(query));
   } catch (e) {
     console.log(e);
     throw Error(e);
-  }
-});
-
-router.get("/test", function (req, res, next) {
-  res.send(
-    db.public.many(
-      `SELECT * FROM PRODUCTS p WHERE p.attribute -> 'rating' -> 'value'::float >= 4.0`
-    )
-  );
-});
-
-/**Paginated results */
-router.get("/products/:page", function (req, res, next) {
-  let offset = (req.params.page - 1) * 10;
-  try {
-    res.send(
-      db.public.many(
-        `SELECT * FROM PRODUCTS ORDER BY ID ASC OFFSET ${offset} ROWS FETCH NEXT 10 ROWS `
-      )
-    );
-  } catch (e) {
-    console.log(e);
   }
 });
 
